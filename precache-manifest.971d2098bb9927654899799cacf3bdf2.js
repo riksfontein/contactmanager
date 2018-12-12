f9ee7f121628a2e9f07b6d69bed2c0f8eeaa43c3
riks Fontein <you@example.com>
2018-12-12 09:44:59 +0100

    Updates

---
+++ precache-manifest.971d2098bb9927654899799cacf3bdf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "f15d8be09c36105b2e25",
+    "url": "/contactmanager/static/js/main.f15d8be0.chunk.js"
+  },
+  {
+    "revision": "b90f77fdb1ae0d151e01",
+    "url": "/contactmanager/static/js/1.b90f77fd.chunk.js"
+  },
+  {
+    "revision": "f15d8be09c36105b2e25",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "b90f77fdb1ae0d151e01",
+    "url": "/contactmanager/static/css/1.18ad024a.chunk.css"
+  },
+  {
+    "revision": "a5351d814306d9da907ad57dfff8c71d",
+    "url": "/contactmanager/index.html"
+  }
+];
